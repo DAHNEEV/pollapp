43ea90b1866c0c73418468d4e32b7357357adffb
kanrisha <dahnev@protonmail.com>
2025-07-10 17:53:01 +0200

    create polls schema

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,6 +1,30 @@
-import { sqliteTable, integer } from 'drizzle-orm/sqlite-core';
+import { sqliteTable, integer, text } from 'drizzle-orm/sqlite-core';
+import { generateUniqueString } from '$lib/utils.js';
 
-export const user = sqliteTable('user', {
-	id: integer('id').primaryKey(),
-	age: integer('age')
+export const polls = sqliteTable('polls', {
+	id: integer().primaryKey({ autoIncrement: true }),
+	slug: text().$default(() => generateUniqueString()),
+	createdAt: integer('created_at', { mode: 'timestamp' }).notNull(),
+	expiresAt: integer('expires_at', { mode: 'timestamp' }).notNull()
+});
+
+export const pollOptions = sqliteTable('poll_options', {
+	id: integer().primaryKey({ autoIncrement: true }),
+	pollId: integer('poll_id')
+		.notNull()
+		.references(() => polls.id, { onDelete: 'cascade' }),
+	text: text('text').notNull(),
+	votes: integer('votes').notNull().default(0)
+});
+
+export const pollVotes = sqliteTable('poll_votes', {
+	id: integer().primaryKey({ autoIncrement: true }),
+	pollId: integer('poll_id')
+		.notNull()
+		.references(() => polls.id, { onDelete: 'cascade' }),
+	optionId: integer('option_id')
+		.notNull()
+		.references(() => pollOptions.id, { onDelete: 'cascade' }),
+	ip: text('ip'),
+	votedAt: integer('voted_at', { mode: 'timestamp' }).notNull()
 });
---
+++ src/lib/utils.ts
@@ -0,0 +1,5 @@
+import { nanoid } from 'nanoid';
+
+export function generateUniqueString(): string {
+	return nanoid();
+}
