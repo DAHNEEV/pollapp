c948314c909e584d47a30189847887d860610a5c
kanrisha <dahnev@protonmail.com>
2025-07-13 22:18:28 +0200

    use nanoid as id instead of autoicrement int

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -2,27 +2,32 @@ import { sqliteTable, integer, text } from 'drizzle-orm/sqlite-core';
 import { nanoid } from 'nanoid';
 
 export const polls = sqliteTable('polls', {
-	id: integer().primaryKey({ autoIncrement: true }),
-	slug: text().$default(() => nanoid()),
+	id: text()
+		.primaryKey()
+		.$default(() => nanoid()),
 	question: text('question').notNull(),
 	createdAt: integer('created_at', { mode: 'timestamp' }).notNull(),
 	expiresAt: integer('expires_at', { mode: 'timestamp' }).notNull()
 });
 
 export const pollOptions = sqliteTable('poll_options', {
-	id: integer().primaryKey({ autoIncrement: true }),
-	pollId: integer('poll_id')
+	id: text()
+		.primaryKey()
+		.$default(() => nanoid()),
+	pollId: text('poll_id')
 		.notNull()
 		.references(() => polls.id, { onDelete: 'cascade' }),
 	text: text('text').notNull()
 });
 
 export const pollVotes = sqliteTable('poll_votes', {
-	id: integer().primaryKey({ autoIncrement: true }),
-	pollId: integer('poll_id')
+	id: text()
+		.primaryKey()
+		.$default(() => nanoid()),
+	pollId: text('poll_id')
 		.notNull()
 		.references(() => polls.id, { onDelete: 'cascade' }),
-	optionId: integer('option_id')
+	optionId: text('option_id')
 		.notNull()
 		.references(() => pollOptions.id, { onDelete: 'cascade' }),
 	ip: text('ip'),
